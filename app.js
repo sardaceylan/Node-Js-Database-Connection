994f2c904d2aa6a547b8c06744d6a5c838d79b9b
sardaceylan <arda_ceylanrnwr@yahoo.com>
2022-02-27 16:21:16 +0300

    main

---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const app = express();
+
+const bodyParser = require('body-parser');
+const path = require('path');
+
+app.set('view engine', 'pug');
+app.set('views', './views');
+
+const connection = require('./utility/database');
+
+const adminRoutes = require('./routes/admin');
+const userRoutes = require('./routes/shop');
+
+const errorController = require('./controllers/errors');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// routes
+app.use('/admin', adminRoutes);
+app.use(userRoutes);
+
+connection.execute("SELECT 5")
+    .then((result) => {
+        console.log(result);
+    }).catch((err) => {
+        console.log(err);
+    });
+
+app.use(errorController.get404Page);
+
+
+app.listen(3001, () => {
+    console.log('listening on port 3000');
+});
---
+++ utility/database.js
@@ -0,0 +1,15 @@
+const mysql = require("mysql2");
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    user: "arda",
+    database: "MyDb",
+    password: "AlAn_TuReNG?@243"
+});
+// const connection = mysql.createConnection({
+//     host: "localhost",
+//     user: "arda",
+//     database: "MyDb",
+//     password: "AlAn_TuReNG?@243"
+// });
+module.exports = connection.promise();
